da2a7d1b99d8193cebf27a8fc8a85a590ae3d197
xio279 <hoangminhvu.2015@gmail.com>
2025-05-16 10:09:36 +0700

    [lab-01]

---
+++ LAB-01/ex04/QuanLySinhVien.py
@@ -0,0 +1,44 @@
+from SinhVien import SinhVien
+
+class QuanLySinhVien:
+    listSinhVien = []
+    
+    def generateID(self):
+        maxId = 1
+        if(self.soLuongSinhVien() > 0):
+            maxId = self.listSinhVien[0]._id
+            for sv in self.listSinhVien:
+                if(maxId < sv._id):
+                    maxId = sv._id
+            maxId = maxId + 1
+        return maxId
+    def soLuongSinhVien(self):
+        return self.listSinhVien.__len__()
+    def nhapSinhVien(self):
+        svId = self.generateID()
+        name = input("Nhập tên sinh viên: ")
+        sex = input("Nhập giới tính sinh viên: ")
+        major = input("Nhập chuyên ngành của sinh viên: ")
+        diemTB = input("Nhập điểm của sinh viên: ")
+        sv = SinhVien(svId, name, sex, major, diemTB)
+        self.xepLoaiHocLuc(sv)
+        self.listSinhVien.append(sv)
+    def updateSinhVien(self, ID):
+        sv:SinhVien = self.findByID(ID)
+        if(sv != None):
+            if(sv != None):
+                name = input("Nhập tên sinh viên: ")
+                sex = input("Nhập giới tính sinh viên: ")
+                major = input("Nhập chuyên ngành của sinh viên: ")
+                diemTB = input("Nhập điểm của sinh viên: ")
+                
+                sv._name = name
+                sv._sex = sex
+                sv._major = major
+                sv._diemTB = diemTB
+                self.xepLoaiHocLuc(sv)
+            else:
+                print("Sinh viên có ID = {} không tồn tại,".format(ID))
+    def sortByID(self):
+        self.listSinhVien.sort(k)
+                
